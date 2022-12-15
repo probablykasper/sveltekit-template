6c83409b7bac7d6edba9750527c581df09f154d8
Kasper <kasperkh.kh@gmail.com>
2022-12-15 04:55:45 +0100

    Update deps

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -6,10 +6,6 @@ module.exports = {
 	overrides: [{ files: ['*.svelte'], processor: 'svelte3/svelte3' }],
 	settings: {
 		'svelte3/typescript': () => require('typescript'),
-		'svelte3/ignore-styles': (attributes) => {
-			// https://github.com/sveltejs/eslint-plugin-svelte3/issues/10
-			return attributes && attributes.lang && attributes.lang !== 'css'
-		},
 	},
 	parserOptions: {
 		sourceType: 'module',
